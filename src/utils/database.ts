2f0fed47a071be90ef4f24853e155acd00dd5b08
Bernard Rodrigues <bernard@bernard.bernard>
2023-02-21 17:51:51 -0300

    Refazendo o .env

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -5,6 +5,7 @@ import { getDatabase, onValue, ref } from 'firebase/database'
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_APIKEY,
     authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    databaseURL: import.meta.env.VITE_DATABASEURL,
     projectId: import.meta.env.VITE_PROJECTID,
     storageBucket: import.meta.env.VITE_STORAGEBUCKET,
     messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
